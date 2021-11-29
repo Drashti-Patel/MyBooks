45563dcbad472afd5006dcb682d72e18efdc3fe3
drashti jasani <patel.drashti400@gmail.com>
2021-11-29 18:44:27 +0200

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Drashti Jaykumar Jasani on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
